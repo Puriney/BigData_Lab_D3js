151a5d8bd960d2668f5fc46ff7179b98d59dcd3d
Puriney <youryanyun@gmail.com>
2016-04-19 09:14:14 -0400

    Finish: bind data to set dimension names and mpg range automatically

--- script/scatter.plot.js
+++ script/scatter.plot.js
@@ -1,4 +1,4 @@
-var margin = { top: 20, right: 20, bottom: 30, left: 40 },
+var margin = { top: 20, right: 50, bottom: 30, left: 50 },
     width = 500 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
@@ -22,13 +22,9 @@ var yAxis = d3.svg.axis()
     .ticks(5)
     .orient("left");
 
-var name = d3.select("body").append("h4", ":first-child")
-    .attr("id", "dotName")
-    .text('Car Name')
 
-
-var svg = d3.select("body").insert("svg", ":first-child")
-    .attr("width", width + margin.left + 2 * margin.right)
+var svg = d3.select("#plot").insert("svg", ":first-child")
+    .attr("width", width + margin.left + margin.right + 10)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -53,31 +49,7 @@ yg
     .attr("dy", ".71em")
     .style("text-anchor", "end");
 
-var legend = svg.selectAll(".legend")
-    .data(color.domain())
-    .enter().append("g")
-    .attr("class", "legend")
-    .attr("transform", function(d, i) {
-        return "translate(0," + i * 20 + ")";
-    });
-
-legend.append("rect")
-    .attr("x", width - 18)
-    .attr("width", 18)
-    .attr("height", 18)
-    .style("fill", color);
-
-legend.append("text")
-    .attr("x", width - 24)
-    .attr("y", 9)
-    .attr("dy", ".35em")
-    .style("text-anchor", "end")
-    .text(function(d) {
-        return d;
-    });
-
-var figLegendRectSize = 18;
-var figLegendSpacing = 4;
+var carname = d3.select("#carname");
 
 
 function update() {
@@ -97,7 +69,7 @@ function update() {
     // select min/max values
     var selectMin = d3.select('#mpg-min').node().value,
         selectMax = d3.select('#mpg-max').node().value;
-    // redraw svg with min/max
+    // filter data with min/max
     data = data.filter(function(d) {
         return d['mpg'] >= selectMin
     });
@@ -112,14 +84,12 @@ function update() {
     y.domain(d3.extent(data, function(d) {
         return d.y;
     })).nice();
-
     xg.call(xAxis);
     yg.call(yAxis);
-
     xg.select("text").text(xVar);
     yg.select("text").text(yVar);
 
-    // on enter
+    // "enter" mode
     var circles = svg.selectAll(".dot")
         .data(data);
 
@@ -129,7 +99,7 @@ function update() {
 
     circles.exit().remove();
 
-    // on update
+    // "update" mode
     circles.attr("cx", function(d) {
             return x(d.x);
         })
@@ -141,17 +111,20 @@ function update() {
         })
         .attr("r", 5);
 
-    circles.on("mouseover", function() {
+    circles.on("mouseover", function(d) {
             d3.select(this)
                 .transition()
                 .duration(10)
-                .attr("r", 8)
+                .attr("r", 8);
+            carname.text("Name of selected car: " + d.name);
         })
         .on("mouseout", function() {
             d3.select(this)
                 .transition()
                 .duration(10)
-                .attr("r", 5)
+                .attr("r", 5);
+            carname.text("Name of selected car: ____");
+
         })
         .append("title")
         .text(function(d) {
@@ -164,41 +137,43 @@ function update() {
                 "\n acceleration: " + d["acceleration"] +
                 "\n model year: " + d["model.year"]
         });
+    // add fig legend
+    var legend = svg.selectAll(".legend")
+        .data(color.domain())
+        .enter()
+        .append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) {
+            return "translate(0," + i * 20 + ")";
+        });
 
-    // add figure legend
+    legend.append("circle")
+        .attr("cx", width)
+        .attr("r", 10)
+        .attr("stroke", "black")
+        .style("fill", color);
 
-    // var figlegend = svg.selectAll(".figlegend")
-    //     .data(color.domain())
-    //     .enter()
-    //     .append("g")
-    //     .attr("class", "legend")
-    //     .attr("transform", function(d, i) {
-    //         var height = figLegendRectSize + figLegendSpacing;
-    //         var offset = height * color.domain().length / 2;
-    //         var horz = width - 2 * margin.right;
-    //         var vert = i * height + 1 * offset;
-    //         return 'translate(' + horz + ',' + vert + ')';
-    //     });
-
-    // figlegend.append("circle")
-    //     .attr("class", "dot")
-    //     .attr("r", 8)
-    //     .style("fill", color)
-    //     .style("stroke", "black");
-
-    // figlegend.append("text")
-    //     .attr("x", figLegendRectSize + figLegendSpacing)
-    //     .attr("y", figLegendRectSize - figLegendSpacing)
-    //     .text(function(d) {
-    //         return d;
-    //     });
+    legend.append("text")
+        .attr("x", width - 15)
+        .attr("y", 0)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text(function(d) {
+            return d;
+        });
 
 }
 
+
+var colNames = null;
 var baseData = null;
+var mpg_data = [];
+
 var dataPath = "data/car.csv";
-d3.csv(dataPath, function(error, csv) {
+d3.csv(dataPath, function(csv) {
+    colNames = d3.keys(csv[0]);
     csv.forEach(function(d) {
+        mpg_data.push(d['mpg']);
         d.mpg = +d.mpg
         d.cylinders = +d.cylinders
         d.displacement = +d.displacement
@@ -208,16 +183,98 @@ d3.csv(dataPath, function(error, csv) {
         d["model.year"] = +d["model.year"]
     });
     baseData = csv;
+    // Set div of webpage layout
+    SetXAxisPlaceholder();
+    SetYAxisPlaceholder();
+    SetMpgRangePlaceholder();
     update("cylinders", "displacement");
 });
 
+function SetXAxisPlaceholder() {
+    var ctlColNames = [];
+    for (var i = 1; i < colNames.length - 1; i++) {
+        ctlColNames.push(colNames[i])
+    };
+    var xChoice = d3.select("#xChoice");
+
+    xChoice.append("b").text("Choose x-Axis: ");
+    xChoice.append("select").attr("name", "xAX").attr("id", "xAXs");
+    xChoice.select("select").selectAll("option")
+        .data(ctlColNames)
+        .enter()
+        .append("option")
+        .attr("value", function(d) {
+            return d;
+        })
+        .text(function(d) {
+            return d;
+        });
+}
+
+function SetYAxisPlaceholder() {
+    var ctlColNames = [];
+    for (var i = 1; i < colNames.length - 1; i++) {
+        ctlColNames.push(colNames[i])
+    };
+    var yChoice = d3.select("#yChoice");
+
+    yChoice.append("b").text("Choose y-Axis: ");
+    yChoice.append("select").attr("name", "yAX").attr("id", "yAXs");
+    yChoice.select("select").selectAll("option")
+        .data(ctlColNames)
+        .enter()
+        .append("option")
+        .attr("value", function(d) {
+            return d;
+        })
+        .text(function(d) {
+            return d;
+        });
+}
+
+function SetMpgRangePlaceholder() {
+    // var mpg_data;
+    var mpg_max = d3.max(mpg_data, function(d) {
+        return +d;
+    });
+    var mpg_min = d3.min(mpg_data, function(d) {
+        return +d;
+    });
+    var mpg_max2 = d3.round(mpg_max * 1.1);
+    var mpg_min2 = d3.round(mpg_min * 0.9);
+
+    var mpgchoice = d3.select("#mpgchoice");
+    mpgchoice.append("p").append("b").text("Set filter based on MPG range: ");
+    mpgchoice.append("span").append("label").attr("for", "mpg-min").text("Min = ");
+    mpgchoice.append("span").append("input")
+        .attr("id", "mpg-min")
+        .attr("type", "number")
+        .attr("value", mpg_min2)
+        .attr("min", mpg_min2)
+        .attr("max", mpg_max2)
+        .attr("step", d3.round((mpg_max2 - mpg_min2) / mpg_data.length));
+    mpgchoice.append("span").text(" ")
+    mpgchoice.append("span").append("label").attr("for", "mpg-max").text("Max = ");
+    mpgchoice.append("span").append("input")
+        .attr("id", "mpg-max")
+        .attr("type", "number")
+        .attr("value", mpg_max2)
+        .attr("min", mpg_min2)
+        .attr("max", mpg_max2)
+        .attr("step", d3.round((mpg_max2 - mpg_min2) / mpg_data.length));
+    mpgchoice.append("span").text(" ")
+
+}
+
+d3.select("#run")
+    .append("button")
+    .attr("id", "update")
+    .text("Click to Query!");
+
+// Listen to event and update
 d3.selectAll('select').on('change', function() {
     update();
 });
-
-// d3.selectAll('input').on('click', function() {
-//     update();
-// })
 
 d3.select('#update').on('click', function() {
     update();
